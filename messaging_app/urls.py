79542e99f4d7cabf23ffe72ceef629a4820a1b36
tallevi120 <68233370+tallevi120@users.noreply.github.com>
2023-07-31 19:34:15 +0300

    fix url

--- messaging_app/urls.py
+++ messaging_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import CustomAuthToken, MessageList, MessageDetail
+
+from .views import CustomAuthToken, MessageList, MessageDetail, UserRegistrationView
 
 urlpatterns = [
     path('messages/', MessageList.as_view(), name='message-list'),
     path('messages/<int:pk>/', MessageDetail.as_view(), name='message-detail'),
-    path('api-token-auth/', CustomAuthToken.as_view(), name='api_token_auth'),
+    path('api/register/', UserRegistrationView.as_view(), name='user-registration'),
+    path('api/api-token-auth/', CustomAuthToken.as_view(), name='api-token-auth'),
 ]
